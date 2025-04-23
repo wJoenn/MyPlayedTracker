eddc7301482f58716ac920f496652983694fcdc0
Louis Ramos <louisramosdev@gmail.com>
2025-04-24 00:06:44 +0200

    fix(time_played_msg: reset startTime when totalPlayedTime is reset

--- MyPlayedTracker.lua
+++ MyPlayedTracker.lua
@@ -8,12 +8,12 @@ f:SetScript("OnEvent", function(self, event, ...)
     if event == "PLAYER_LOGIN" then
         RequestTimePlayed() -- Fires a TIME_PLAYED_EVENT when completed
 
-        self.startTime = GetTime()
         self.startTimestamp = date("%Y-%m-%d")
 
     elseif event == "TIME_PLAYED_MSG" then
         local totalPlayedTime = ... -- desconstructs the totalPlayedTime from the event handler's spread arguments
         self.totalPlayedTime = totalPlayedTime
+        self.startTime = GetTime()
 
     elseif event == "PLAYER_LOGOUT" then
         local name = UnitName("player")
